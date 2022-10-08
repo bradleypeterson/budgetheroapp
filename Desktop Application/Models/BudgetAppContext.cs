ca94f8a248bd531738b00111f217ad837e7b7bf9
Aric Glanville <aricglanville@mail.weber.edu>
2022-10-07 18:14:08 -0600

    Update BudgetAppContext.cs

--- Desktop Application/Models/BudgetAppContext.cs
+++ Desktop Application/Models/BudgetAppContext.cs
@@ -23,7 +23,8 @@ namespace Desktop_Application.Models
         {
             var folder = Environment.SpecialFolder.LocalApplicationData;
             var path = Environment.GetFolderPath(folder);
-            DbPath = System.IO.Path.Join(path, "budgetherodesktop.db");            
+            DbPath = System.IO.Path.Join(path, "budgetherodesktop.db");
+            Database.EnsureCreated();
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
