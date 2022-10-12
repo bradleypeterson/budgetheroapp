0bcfe192c1e7b3a9a62b82a64849ba7280864480
DevOlson <rollin.j.olson@outlook.com>
2022-10-11 23:32:14 -0400

    Create basic unit tests fro User table CRUD actions

--- UnitTests/TestHelpers/ModelFactory.cs
+++ UnitTests/TestHelpers/ModelFactory.cs
@@ -13,14 +13,53 @@ namespace UnitTests.TestHelpers
         {
             return new User()
             {
-                FirstName = "John",
-                LastName = "Doe",
-                EmailAddress = "jdoe@example.com",
+                FirstName =     "John",
+                LastName =      "Doe",
+                EmailAddress =  "jdoe@example.com",
                 PercentageMod = 20,
-                Username = "doeman",
-                Password = "123456",
+                Username =      "doeman",
+                Password =      "123456",
                 UserImageLink = "https://www.fakepicturelink.com/the_doeman.jpg"
             };
+        }
+
+        public static List<User> CreateUsers()
+        {
+            List<User> users = new()
+            {
+                new User()
+                {
+                    FirstName =     "Michael",
+                    LastName =      "Scott",
+                    EmailAddress =  "mscott@dundermifflinpaperco.com",
+                    PercentageMod = 50,
+                    Username =      "agentscarn",
+                    Password =      "hackme123",
+                    UserImageLink = "https://www.fakepicturelink.com/agent_scarn.jpg"
+                },
+                new User()
+                {
+                    FirstName =     "Dwight",
+                    LastName =      "Schrute",
+                    EmailAddress =  "dshrute@dundermifflinpaperco.com",
+                    PercentageMod = 30,
+                    Username =      "beatfarmer",
+                    Password =      "ilovepaper",
+                    UserImageLink = "https://www.fakepicturelink.com/beat_farmer.jpg"
+                },
+                new User()
+                {
+                    FirstName =     "Jim",
+                    LastName =      "Halpert",
+                    EmailAddress =  "jhalpert@dundermifflinpaperco.com",
+                    PercentageMod = 10,
+                    Username =      "tuna",
+                    Password =      "dwightisajoke",
+                    UserImageLink = "https://www.fakepicturelink.com/bored.jpg"
+                },
+            };
+
+            return users;
         }
     }
 }
