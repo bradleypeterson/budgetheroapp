bf73fa71c932f5491924ee565da20319b0eef2d1
Rollin Olson <rollin.j.olson@outlook.com>
2022-10-25 17:06:51 -0400

    Create a basic unit test for the APIServiceManager

---
+++ UnitTests/APIUnitTests.cs
@@ -0,0 +1,35 @@
+﻿using ModelsLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTests.TestHelpers;
+using Xunit.Abstractions;
+
+namespace UnitTests
+{
+    public class APIUnitTests
+    {
+        private readonly ITestOutputHelper output;
+
+        public APIUnitTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public async Task GetUserAsync()
+        {
+            // Arrange
+            User newUser = ModelFactory.CreateUser();
+            newUser.UserId = 2;
+
+            // Act
+            User existingUser = await APIServiceManager.GetUserAsync(newUser);
+
+            // Assert
+            Assert.True(existingUser.UserId == newUser.UserId);
+        }
+    }
+}
