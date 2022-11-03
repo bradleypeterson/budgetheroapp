b5bfab974d799ca6dce6b0e7dad540a3a93dd950
tevindayley <89816221+tevindayley@users.noreply.github.com>
2022-11-02 18:27:52 -0600

    Added login code so it takes you to the dashboard.

--- DesktopApplication/ViewModels/LoginViewModel.cs
+++ DesktopApplication/ViewModels/LoginViewModel.cs
@@ -8,10 +8,12 @@ namespace DesktopApplication.ViewModels;
 public class LoginViewModel : ObservableRecipient
 {
     public ICommand NavigateCommand { get; }
+    public ICommand DashboardCommand { get; }
 
     public LoginViewModel()
     {
         var _navigationService = App.GetService<INavigationService>();
         NavigateCommand = new NavigateCommand(_navigationService, typeof(RegistrationViewModel).FullName!);
+        DashboardCommand = new NavigateCommand(_navigationService, typeof(AccountsViewModel).FullName!);
     }
 }
