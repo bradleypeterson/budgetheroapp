05e81be8bd665e01324836e6e60d20ceba5bec0a
DevOlson <rollin.j.olson@outlook.com>
2022-11-09 10:30:17 -0500

    Delete .editorconfig

---
+++ DesktopApplication/Models/ObservableTransaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using ModelsLibrary;
+
+namespace DesktopApplication.Models;
+public class ObservableTransaction : ObservableObject
+{
+    private readonly Transaction _transaction;
+
+    public ObservableTransaction(Transaction transaction)
+    {
+        _transaction = transaction;
+    }
+}
