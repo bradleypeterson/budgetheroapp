90eac2cfa38d4563127c5eff6614c13d904b0d1f
DevOlson <65735493+DevOlson@users.noreply.github.com>
2022-11-23 16:19:56 -0500

    Accounts View: Populate Details Pane with Transactions Linked to Selected Account

--- DesktopApplication/ViewModels/AccountsViewModel.cs
+++ DesktopApplication/ViewModels/AccountsViewModel.cs
@@ -7,9 +7,7 @@ using DesktopApplication.Contracts.Services;
 using DesktopApplication.CustomEventArgs;
 using DesktopApplication.Helpers;
 using DesktopApplication.Models;
-using DesktopApplication.ViewModels.Forms;
 using DesktopApplication.Views.Forms;
-using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
 using ModelsLibrary;
 
 namespace DesktopApplication.ViewModels;
@@ -34,8 +32,11 @@ public class AccountsViewModel : ObservableRecipient
     public IAsyncRelayCommand ShowAddDialogCommand { get; }
     public IAsyncRelayCommand ShowEditDialogCommand { get; }
     public IAsyncRelayCommand ShowDeleteDialogCommand { get; }
-    public ObservableCollection<ObservableBankAccount> BankAccounts { get; set; } = new();
 
+    private IEnumerable<Transaction>? allTransactions;
+    public ObservableCollection<ObservableBankAccount> BankAccounts { get; set; } = new();
+    public ObservableCollection<Transaction> AccountTransactions { get; set; } = new();
+    
     private ObservableBankAccount? _selectedBankAccount;
     public ObservableBankAccount? SelectedBankAccount
     {
@@ -43,6 +44,7 @@ public class AccountsViewModel : ObservableRecipient
         set
         {
             SetProperty(ref _selectedBankAccount, value);
+            FilterAccountTransactions(value!.BankAccount.BankAccountId);
             HasItemSelected = value is not null;
         }
     }
@@ -66,6 +68,8 @@ public class AccountsViewModel : ObservableRecipient
                 BankAccounts.Add(new ObservableBankAccount(bankAccount!));
             }
         }
+
+        allTransactions = _dataStore.Transaction.GetAll(t => t.BankAccount.UserId == _sessionService.GetSessionUserId());
     }
 
     private async Task ShowAddDialog()
@@ -143,5 +147,16 @@ public class AccountsViewModel : ObservableRecipient
     {
         BankAccountForm accountForm = (BankAccountForm)e.Content;
         return accountForm.ViewModel.BankAccount;
+    }
+
+    private void FilterAccountTransactions(int accountId)
+    {
+        if (allTransactions is not null)
+        {
+            AccountTransactions.Clear();
+            List<Transaction> accountTransactions = allTransactions.Where(t => t.BankAccountId == accountId)
+                                                                   .OrderByDescending(t => t.TransactionDate).ToList();
+            accountTransactions.ForEach(AccountTransactions.Add);
+        }
     }
 }
