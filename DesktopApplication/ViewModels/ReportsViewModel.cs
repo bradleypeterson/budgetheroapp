09fc2e79e7b1e89846cb75653c5edc5af3abb0b9
tevindayley <89816221+tevindayley@users.noreply.github.com>
2022-11-30 19:38:34 -0700

    Added code getting transactions from the database for the charts in the code behind and the viewmodel.

--- DesktopApplication/ViewModels/ReportsViewModel.cs
+++ DesktopApplication/ViewModels/ReportsViewModel.cs
@@ -1,10 +1,45 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using DesktopApplication.Contracts.Data;
+using DesktopApplication.Contracts.Services;
+using DesktopApplication.Models;
+using System.Collections.ObjectModel;
+using ModelsLibrary;
 
 namespace DesktopApplication.ViewModels;
 
 public class ReportsViewModel : ObservableRecipient
 {
+
+    private readonly ISessionService _sessionService; //session service to find the current user
+    private readonly IDataStore _dataStore; //used to get and save data
+
+    //Observable List of Transactions from the User
+    public ObservableCollection<ObservableTransaction> Transactions { get; set; } = new();
+
+    //Constructor
     public ReportsViewModel()
     {
+        _sessionService = App.GetService<ISessionService>();
+        _dataStore = App.GetService<IDataStore>();
+    }
+
+
+    //Used to get the user and the user's transactions
+    public async Task LoadAsync()
+    {
+        int userId = _sessionService.GetSessionUserId();
+
+        if (Transactions.Any()) return;
+        IEnumerable<Transaction?> transactions =
+            await _dataStore.Transaction.ListAsync(t => t.BankAccount.UserId == userId, null!, "BankAccount,BudgetCategory");
+
+        if (transactions is not null)
+        {
+            foreach (Transaction? transaction in transactions)
+            {
+                if (transaction is not null && transaction.BankAccount.UserId == userId)
+                    Transactions.Add(new ObservableTransaction(transaction));
+            }
+        }
     }
 }
--- DesktopApplication/Views/ReportsPage.xaml.cs
+++ DesktopApplication/Views/ReportsPage.xaml.cs
@@ -1,20 +1,31 @@
-﻿using DesktopApplication.ViewModels;
-
+﻿using DesktopApplication.Contracts.Data;
+using DesktopApplication.Contracts.Services;
+using DesktopApplication.Models;
+using DesktopApplication.Services;
+using DesktopApplication.ViewModels;
 using Microsoft.UI.Xaml.Controls;
 using Syncfusion.UI.Xaml.Charts;
+using System.Collections.ObjectModel;
 
 namespace DesktopApplication.Views;
 
 public sealed partial class ReportsPage : Page
 {
-    public ReportsViewModel ViewModel
-    {
-        get;
-    }
+
+    public ReportsViewModel ViewModel { get; }
 
     public ReportsPage()
     {
         ViewModel = App.GetService<ReportsViewModel>();
         InitializeComponent();
+
     }
+
+    //Once the page is loaded this function will call the LoadAsync which will get the data
+    private async void Page_Loaded(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        await ViewModel.LoadAsync();
+    }
+
+
 }
