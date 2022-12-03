22697c45990a9f9b4d60693099d45fbe2fa5cccf
DevOlson <rollin.j.olson@outlook.com>
2022-12-03 16:29:04 -0500

    API: Allow users to login from a new computer

---
+++ DebugTools/Jsonizer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DebugTools
+{
+    public class Jsonizer
+    {
+        public static void GimmeDatJson<T>(T obj)
+        {
+            var opt = new JsonSerializerOptions() { 
+                WriteIndented = true,
+                ReferenceHandler = ReferenceHandler.Preserve
+            };
+
+            string strJson = JsonSerializer.Serialize(obj, opt);
+            Debug.WriteLine(strJson);
+        }
+    }
+}
--- DesktopApplication/ViewModels/LoginViewModel.cs
+++ DesktopApplication/ViewModels/LoginViewModel.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using System.Windows.Input;
+﻿using System.Windows.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using DesktopApplication.Contracts.Data;
@@ -14,6 +13,7 @@ public class LoginViewModel : ObservableRecipient
     private readonly ISessionService _sessionService;
     private readonly IDataStore _dataStore;
     private readonly IPasswordService _passwordService;
+    private readonly IAPIService _apiService;
 
     public LoginViewModel()
     {
@@ -21,6 +21,8 @@ public class LoginViewModel : ObservableRecipient
         _sessionService = App.GetService<ISessionService>();
         _dataStore = App.GetService<IDataStore>();
         _passwordService = App.GetService<IPasswordService>();
+        _apiService = App.GetService<IAPIService>();
+
         SignUpCommand = new RelayCommand(NavigateToSignUpView);
         LoginCommand = new AsyncRelayCommand(Login);
         IsFormComplete = false;
@@ -65,29 +67,41 @@ public class LoginViewModel : ObservableRecipient
 
     private async Task Login()
     {
+        // First check the local database for an existing user.
         User? existingUser = await _dataStore.User.GetAsync(u => u.Username == _username);
 
+        // If existing user is not in the local database, make an API call.
+        if (existingUser is null)
+        {
+            IEnumerable<User>? users = await _apiService.GetAsync<IEnumerable<User>>("users");
 
-            if (existingUser is not null)
+            if (users is not null)
+                existingUser = users.FirstOrDefault(u => u.Username == Username);
+
+                if (existingUser is not null)
+                    await _dataStore.User.AddAsync(existingUser);
+        }
+
+        if (existingUser is not null)
+        {
+            bool isValidPassword = _passwordService.VerifyPassword(_password!, existingUser.Password!);
+
+            if (isValidPassword)
             {
-                bool isValidPassword = _passwordService.VerifyPassword(_password!, existingUser.Password!);
-
-                if (isValidPassword)
-                {
-                    OnValidLogin?.Invoke(this, EventArgs.Empty);
-                    await Task.Delay(1500);
-                    _sessionService.CreateSession(existingUser);
-                    _navigationService.NavigateTo(typeof(AccountsViewModel).FullName!);
-                }
-                else
-                {
-                    OnUserNotFound?.Invoke(this,EventArgs.Empty);
-                }
+                OnValidLogin?.Invoke(this, EventArgs.Empty);
+                await Task.Delay(1500);
+                _sessionService.CreateSession(existingUser);
+                _navigationService.NavigateTo(typeof(AccountsViewModel).FullName!);
             }
             else
             {
-                OnUserNotFound?.Invoke(this, EventArgs.Empty);
+                OnUserNotFound?.Invoke(this,EventArgs.Empty);
             }
+        }
+        else
+        {
+            OnUserNotFound?.Invoke(this, EventArgs.Empty);
+        }
         
     }
 
